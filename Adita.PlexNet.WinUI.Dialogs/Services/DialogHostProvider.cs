05421292625b552a4e6fda6d6ec2b95684f20876
Setya Adi Kurnia <setya.adi@weq-tech.com>
2024-09-11 18:17:48 +0700

    Add project files.

---
+++ Adita.PlexNet.WinUI.Dialogs/Services/DialogHostProvider.cs
@@ -0,0 +1,31 @@
+﻿using Adita.PlexNet.Core.Dialogs;
+using Microsoft.UI.Xaml;
+
+namespace Adita.PlexNet.WinUI.Dialogs.Services
+{
+    /// <summary>
+    /// Represents a dialog host provider.
+    /// </summary>
+    public class DialogHostProvider : IDialogHostProvider
+    {
+        #region Private fields
+        private Window? MainWindow;
+        #endregion Private fields
+
+        #region Public methods
+        /// <summary>
+        /// Sets the host of the dialog using specified <paramref name="window"/>.
+        /// </summary>
+        /// <param name="window">The <see cref="Window"/> for the dialog host.</param>
+        public void SetHost(Window window)
+        {
+            MainWindow = window;
+        }
+        /// <inheritdoc/>
+        public object? GetHost<TDialog>()
+        {
+            return MainWindow;
+        }
+        #endregion Public methods
+    }
+}
